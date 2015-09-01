0a2464ae3302e35aa02919683debcdbf53a638df
liangtengfei <2916288723@qq.com>
2015-09-01 17:59:52 +0800

    准备加入ajax和json传递数据

--- src/com/calf/daily/action/RecorderController.java
+++ src/com/calf/daily/action/RecorderController.java
@@ -3,6 +3,7 @@ package com.calf.daily.action;
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -10,11 +11,11 @@ import com.calf.daily.interf.RecorderMapper;
 import com.calf.daily.model.Recorder;
 
 @Controller
-@RequestMapping("/user.sm")
+@RequestMapping("/user")
 public class RecorderController {
 	private RecorderMapper recordService;
 	
-	@RequestMapping(value="/")
+	@RequestMapping(value="/", method=RequestMethod.GET)
 	public String toIndex(){
 		return "redirect:index.html";
 	}
@@ -22,12 +23,34 @@ public class RecorderController {
 	public String userLogin(Recorder user){
 		Recorder r=recordService.selectByNamePass(user);
 		if(r==null){
+			return "redirect:login.html";
+		}
+		return "index";
+	}
+	@RequestMapping(params="m=regist", method = RequestMethod.POST)
+	public String userRegist(Recorder user){
+		int result=recordService.insertSelective(user);
+		if(result==0){
+			return "redirect:regist.html";
+		}
+		return "index";
+	}
+	/*@RequestMapping(params="m=login", method = RequestMethod.POST)
+	public String userLogin(Recorder user){
+		Recorder r=recordService.selectByNamePass(user);
+		if(r==null){
 			return "redirect:index.html";
 		}
 		return "index";
 	}
-	
-	
+	@RequestMapping(params="m=login", method=RequestMethod.POST)
+	public String userRegist(){
+		return "";
+	}*/
+	@RequestMapping(value="/record/{record_id}", method=RequestMethod.GET)
+	public String getRecordById(@PathVariable("record_id")String id){
+		return "";
+	}
 	public RecorderMapper getRecordService() {
 		return recordService;
 	}
